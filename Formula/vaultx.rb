6dfac74752b16d117d633317d736ba845405ca86
yacasper <kalashnikov.by@gmail.com>
2026-05-18 20:06:11 +0000

    Update vaultx formula to v1.0.2

--- Formula/vaultx.rb
+++ Formula/vaultx.rb
@@ -1,8 +1,8 @@
 class Vaultx < Formula
   desc "Cross-platform file encryption utility — AES-256-GCM / ChaCha20-Poly1305 + Argon2id"
   homepage "https://github.com/yacasper/vaultx"
-  url "https://github.com/yacasper/vaultx/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "f10ca2fcd9d1ccee2772a703fa88761ba0934499f018597ac31c5bc912f84bdf"
+  url "https://github.com/yacasper/vaultx/archive/refs/tags/v1.0.2.tar.gz"
+  sha256 "f1ff6aa5ad6c88dce092a60caf74cbeeb95d1c0f73b7129adba76525e43ee2a1"
   license "MIT"
 
   depends_on "go" => :build
